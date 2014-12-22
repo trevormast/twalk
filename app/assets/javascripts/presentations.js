7d31d1472e4d4e3a18c76dc8bcf5ab13e07dbbac
amoose <amos@amosarts.com>
2014-12-21 16:01:32 -0800

    More

--- app/assets/javascripts/presentations.js
+++ app/assets/javascripts/presentations.js
@@ -1,11 +1,14 @@
 //= require jquery
 //= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
 //= require websocket_rails/main
 
+window.Twalk2 = Ember.Application.create()
 
 $(function () {
-  var max = parseInt($(window).height() * 0.8);
-  $('section img').css('max-height',max+'px');
+  
 
 
   $('.fadeIn').each(function(i) {
@@ -18,5 +21,26 @@ $(function () {
   }).mouseleave(function () {
     $(this).removeClass('expanded');
   });
-    
+  
+
+  $('.flash-messages').change( function () {
+    $('.flash-messages').find('.alert').each(function () {
+      $(this).delay(500).fadeOut();
+    });
+  });
+
+  $('.flash-messages').find('.alert').each(function () {
+    $(this).delay(4000).fadeOut();
+  });
+
+  $(window).resize(function () {
+    windowAdjust();
+  });
 });
+
+
+function windowAdjust() {
+  var max = parseInt($(window).height() * 0.8);
+  $('section img').css('max-height',max+'px');
+
+}
